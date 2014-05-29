3959dfcd823b199127c416719d345e9f7bb186a3
Brittany Mazza <bmazza555@gmail.com>
2014-05-28 23:27:08 -0700

    Remove extra not necessary lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ SimpleCov.start 'rails'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'stripe_mock'
-require 'capybara/rails'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
